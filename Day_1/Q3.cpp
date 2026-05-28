7827cb6b976f59bc387d8b5228d025676a4e2423
Shresthkhandelwal1311 <khandelwalshresth06@gmail.com>
2026-05-28 21:35:35 +0530

    Create Q3.cpp

---
+++ Day_1/Q3.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int i,fac=1;
+    cout<<"enter the number:";
+    cin>>i;
+    while(i>=1){
+        fac=fac*i;
+        i--;
+    }
+    cout<<"the factorial of a entered number is:"<<fac;
+    return 0;
+
+}
