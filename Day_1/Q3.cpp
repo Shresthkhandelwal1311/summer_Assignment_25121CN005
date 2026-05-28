821ae57f3b6969eface82567da478edfc075c9cd
Shresthkhandelwal1311 <khandelwalshresth06@gmail.com>
2026-05-28 21:31:02 +0530

    Create Q3.cpp

---
+++ Day_1/Q3.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n,d=0;
+    cout<<"enter the number:";
+    cin>>n;
+    while(n!=0){
+        n=n/10;
+        d++;
+    }
+    cout<<"total number of digits are:"<<d;
+    return 0;
+}
