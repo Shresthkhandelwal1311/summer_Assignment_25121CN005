2074d0a30929892fdc8e2a2894d33b6deeb3d94e
Shresthkhandelwal1311 <khandelwalshresth06@gmail.com>
2026-05-28 21:26:01 +0530

    Update Q1.cpp

--- Day_1/Q1.cpp
+++ Day_1/Q1.cpp
@@ -1,1 +1,12 @@
-
+#include<iostream>
+using nam#espace std;
+int main(){
+    int n,sum=0;
+    cout<<"enter the number n:";
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        sum=sum+i;
+    }
+    cout<<"sum"<<sum;
+    return 0;
+}
