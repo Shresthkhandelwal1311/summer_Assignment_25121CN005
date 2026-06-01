8ae2fd37c9e996d40d5d349e815fed4ca43a127a
Shresthkhandelwal1311 <khandelwalshresth06@gmail.com>
2026-06-01 18:30:01 +0530

    Update Q1.cpp

--- Day_1/Q1.cpp
+++ Day_1/Q1.cpp
@@ -1,1 +1,13 @@
-
+//Write a program to Calculate sum of first N natural numbers. 
+#include<iostream>
+using namespace std;
+int main(){
+    int n,sum=0;
+    cout<<"enter the number n:";
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        sum=sum+i;
+    }
+    cout<<"sum"<<sum;
+    return 0;
+}
