54300306418cbd1da2a2e7304863df84c5ec1446
Shresthkhandelwal1311 <khandelwalshresth06@gmail.com>
2026-06-01 18:33:33 +0530

    Update Q3.cpp

--- Day_1/Q3.cpp
+++ Day_1/Q3.cpp
@@ -1,1 +1,15 @@
+//Write a program to Find factorial of a number.
+#include<iostream>
+using namespace std;
+int main(){
+    int i,fac=1;
+    cout<<"enter the number:";
+    cin>>i;
+    while(i>=1){
+        fac=fac*i;
+        i--;
+    }
+    cout<<"the factorial of a entered number is:"<<fac;
+    return 0;
 
+}
