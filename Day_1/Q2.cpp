6d76d63e656afef4af6208acad210ca791acd96a
Shresthkhandelwal1311 <khandelwalshresth06@gmail.com>
2026-06-01 18:32:01 +0530

    Update Q2.cpp

--- Day_1/Q2.cpp
+++ Day_1/Q2.cpp
@@ -1,1 +1,12 @@
-
+//Write a program to Print multiplication table of a given number. 
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter the number:";
+    cin>>n;
+    for(int i=1;i<=10;i++){
+        cout<<n<<"x"<<i<<"="<<n*i<<endl;
+    }
+    return 0;
+}
