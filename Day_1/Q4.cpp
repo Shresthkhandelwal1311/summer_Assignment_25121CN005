3e8cc8f5dbfabec951f50d800c27d3ac27375fed
Shresthkhandelwal1311 <khandelwalshresth06@gmail.com>
2026-06-01 18:35:10 +0530

    Update Q4.cpp

--- Day_1/Q4.cpp
+++ Day_1/Q4.cpp
@@ -1,1 +1,14 @@
-
+//Write a program to Count digits in a number. 
+#include<iostream>
+using namespace std;
+int main(){
+    int n,d=0;
+    cout<<"enter the number:";
+    cin>>n;
+    while(n!=0){
+        n=n/10;
+        d++;
+    }
+    cout<<"total number of digits are:"<<d;
+    return 0;
+}
