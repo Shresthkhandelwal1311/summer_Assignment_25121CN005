dbad02c51c0babaaf463903f54f085eeec7e0869
Shresthkhandelwal1311 <khandelwalshresth06@gmail.com>
2026-06-02 12:48:54 +0530

    Update Q7.cpp

--- Day_2/Q7.cpp
+++ Day_2/Q7.cpp
@@ -1,1 +1,14 @@
-
+//Write a program to Find product of digits. 
+#include<iostream>
+using namespace std;
+int main(){
+    int i,pro=1;
+    cout<<"enter the no.:";
+    cin>>i;
+    while(i!=0){
+        pro=pro*(i%10);
+        i=i/10;
+    }
+    cout<<"product is:"<<pro;
+    return 0;
+}
