813a21591d318e3fa16999ea120e455024f8823a
Shresthkhandelwal1311 <khandelwalshresth06@gmail.com>
2026-06-02 12:47:03 +0530

    Update Q6.cpp

--- Day_2/Q6.cpp
+++ Day_2/Q6.cpp
@@ -1,1 +1,14 @@
-
+//Write a program to Reverse a number.
+#include<iostream>
+using namespace std;
+int main(){
+    int i,rev=0;
+    cout<<"enter the no.:";
+    cin>>i;
+    while(i>0){
+        rev=(rev*10)+i%10;
+        i=i/10;
+    }
+    cout<<"reverse is:"<<rev;
+    return 0;
+}
