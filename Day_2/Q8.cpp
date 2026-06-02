1573f69babc1bf8b9bbf2453c4a4b7fc25e9745e
Shresthkhandelwal1311 <khandelwalshresth06@gmail.com>
2026-06-02 12:50:42 +0530

    Update Q8.cpp

--- Day_2/Q8.cpp
+++ Day_2/Q8.cpp
@@ -1,1 +1,20 @@
-
+//Write a program to Check whether a number is palindrome. 
+#include<iostream>
+using namespace std;
+int main(){
+    int i,n,rev=0;
+    cout<<"enter the no.:";
+    cin>>i;
+    n=i;
+    while(i>0){
+        rev=(rev*10)+i%10;
+        i=i/10;
+    }
+    if(rev==n){
+        cout<<"palindrome";
+    }
+    else{
+        cout<<"not palindrome";
+    }
+    return 0;
+}
