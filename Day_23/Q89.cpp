6f833db8418a49103866d69c3e237b463975d163
Shresthkhandelwal1311 <khandelwalshresth06@gmail.com>
2026-06-23 10:46:11 +0530

    Add files via upload

--- Day_23/Q89.cpp
+++ Day_23/Q89.cpp
@@ -1,1 +1,22 @@
+//Write a program to Find first non-repeating character.
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+    string str;
+    cout<<"Enter a string: ";
+    cin>>str;
 
+    int freq[256]={0};
+    for(char ch:str){
+        freq[ch]++;
+    }
+    for(char ch:str){
+        if(freq[ch]==1){
+            cout<<"First non-repeating character: "<<ch<<endl;
+            return 0;
+        }
+    }
+    cout<<"No non-repeating character found"<<endl;
+    return 0;
+}
