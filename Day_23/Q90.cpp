f45ed394bb435decbdc43df8fd5e35279a124344
Shresthkhandelwal1311 <khandelwalshresth06@gmail.com>
2026-06-23 10:47:35 +0530

    Update Q90.cpp

--- Day_23/Q90.cpp
+++ Day_23/Q90.cpp
@@ -1,1 +1,19 @@
-
+//Write a program to Find first repeating character. 
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+    string str;
+    cout<<"Enter a string: ";
+    cin>>str;
+    int freq[256]={0};
+    for(char ch:str){
+        freq[ch]++;
+        if(freq[ch]==2){
+            cout<<"First repeating character: "<<ch<<endl;
+            return 0;
+        }
+    }
+    cout<<"No repeating character found"<<endl;
+    return 0;
+}
